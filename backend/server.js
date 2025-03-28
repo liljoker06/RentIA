838b6d23ae4e8f5ac5a7eccc55e761fc374f57ea
liljoker06 <lahrimialadin@gmail.com>
2025-03-28 15:22:31 +0100

    Changement du port du serveur de 3000 à 5000

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dotenv = require("dotenv")
 const cors = require("cors")
 const app = express()
-const port = 3000
+const port = 5000
 const sequelize = require('./config/database'); 
 
 dotenv.config();
